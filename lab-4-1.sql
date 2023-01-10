a31c770ae6e4de725015362d48882251e08cd640
CMaldonado13 <christian.maldonado1@kellogg.northwestern.edu>
2023-01-10 22:23:50 +0000

    did lab 4 again

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -6,5 +6,11 @@
 
 SELECT SUM(stats.hits)
 FROM stats INNER JOIN players 
-ON players.id = stats.player_id 
+ON players.id = stats.player_id
 WHERE first_name = "Barry" AND last_name = "Bonds";
+/*
+SELECT teams.year, teams.name, stats.hits
+FROM stats INNER JOIN players 
+ON players.id = stats.player_id INNER JOIN teams ON stats.team_id = teams.id
+WHERE first_name = "Barry" AND last_name = "Bonds";
+*/
